58fbf6d254fe4d6e5c79c5080c86bf0ae35b3e1c
JAVA <JAVA@DESKTOP-N5TLAJD>
2025-12-01 10:43:49 +0530

    for selenium

---
+++ src/main/java/com/mycompany/abhiram8/Abhiram8.java
@@ -0,0 +1,24 @@
+package com.mycompany.abhiram8;
+
+public class Abhiram8 {
+
+    public void readArrayOddSum(){
+        int data[] = {45, 55, 65, 12, 33, 78};
+        int sum = 0;
+        
+        
+        for(int i = 0; i < data.length; i++) {
+            if(data[i] % 2 != 0) { 
+                sum = sum + data[i];
+            }
+        }
+        
+     
+        System.out.println("Sum of odd numbers is: " + sum);
+    }
+    
+    public static void main(String[] args) {
+        Abhiram8 obj = new Abhiram8();
+        obj.readArrayOddSum(); 
+    }
+}
